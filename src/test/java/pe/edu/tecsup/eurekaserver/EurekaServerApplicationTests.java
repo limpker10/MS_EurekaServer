52af81e64e953e24e5046efb39ddead5797fdbdd
Jose Cano <jcanov@unsa.edu.pe>
2023-09-23 07:28:39 -0500

    Generando proyecto EurekaServer

---
+++ src/test/java/pe/edu/tecsup/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
